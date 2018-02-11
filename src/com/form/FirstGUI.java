7f852b4e9ecde117968c2eb285084bcb31ae7b65
Mr.Admin <jazscripts@gmail.com>
2018-02-11 21:37:02 +0400

    first commit

--- src/com/form/FirstGUI.java
+++ src/com/form/FirstGUI.java
@@ -2,25 +2,40 @@ package com.form;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class FirstGUI extends JFrame {
     private JButton referButton=new JButton("Reference");
     private JTextArea inputTextArea =new JTextArea();
-    private JScrollPane inputArea = new JScrollPane(inputTextArea);
     private JLabel inputLabel=new JLabel("input:");
     private JLabel outputLabel=new JLabel("output:");
     private JTextArea outputTextArea= new JTextArea();
-    private JScrollPane outputArea = new JScrollPane(outputTextArea);
     public FirstGUI(){
         super("Comp0l");
         this.setBounds(200,200,500,600);
+        inputTextArea.setLineWrap(true);
+        inputTextArea.setWrapStyleWord(true);
+        outputTextArea.setLineWrap(true);
+        outputTextArea.setWrapStyleWord(true);
+        inputTextArea.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                String inpText=inputTextArea.getText();
+                if (e.getKeyCode() != KeyEvent.VK_BACK_SPACE) {
+                    outputTextArea.setText(inpText + e.getKeyChar());
+                }
+                else if(inpText.length()>0) outputTextArea.setText(inpText.substring(0,inpText.length()-1));
+                }
+        });
+        JScrollPane inputArea = new JScrollPane(inputTextArea);
+        JScrollPane outputArea = new JScrollPane(outputTextArea);
         inputArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        inputArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        inputArea.setPreferredSize(new Dimension(400,300));
+        inputArea.setPreferredSize(new Dimension(300,300));
         outputArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        outputArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        outputArea.setPreferredSize(new Dimension(400,180));
+        outputArea.setPreferredSize(new Dimension(300,180));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         Container container =this.getContentPane();
         LayoutManager layout = new BoxLayout(container, BoxLayout.Y_AXIS);
         container.setLayout(layout);
@@ -29,5 +44,7 @@ public class FirstGUI extends JFrame {
         container.add(outputLabel);
         container.add(outputArea);
     }
+
+
 
 }
